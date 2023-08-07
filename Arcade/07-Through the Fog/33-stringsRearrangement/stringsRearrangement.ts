6ce674d825091d4bd4f568b2ec5226c370f911d1
Patrick Melo <patrickabimael@yahoo.com.br>
2023-08-07 16:40:47 -0300

    cria arquivo de resolução do desafio 33

---
+++ Arcade/07-Through the Fog/33-stringsRearrangement/stringsRearrangement.ts
@@ -0,0 +1,13 @@
+function solution(inputArray: string[]): boolean {
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    for (let j = 0; j < inputArray[i].length; j++) {
+
+    }
+  }
+  return false
+}
+
+const input = ["abc", "abx", "axx", "abx", "abc"]
+
+console.log(solution(input))
+// solution(input)
